063bfe763d4dc34dd95d74483d7fc8c768cc32df
yogaprastyoo <yogaprastyobayu@gmail.com>
2025-05-23 16:37:02 +0700

    feat: fix style tailwindcss

--- src/layouts/user/header.php
+++ src/layouts/user/header.php
@@ -8,10 +8,12 @@ require_once __DIR__ . '/../../../config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= $base_url ?>/src/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     <title><?= $title ?? 'USER'; ?></title>
     <style>
         @import url("https://fonts.cdnfonts.com/css/product-sans");
+
         main {
             font-family: "Product Sans", sans-serif;
         }
